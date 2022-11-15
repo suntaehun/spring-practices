0f9b474a3eceee26b3f8363ca125266022863301
suntaehun <sth7862@naver.com>
2022-11-15 10:49:43 +0900

    @RequestMapping

---
+++ helloweb03/src/main/java/com/bitacademy/helloweb/controller/BoardController.java
@@ -0,0 +1,24 @@
+package com.bitacademy.helloweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+//	@RequestMapping 메소드 단독 매핑
+
+@Controller
+public class BoardController {
+	
+	@ResponseBody
+	@RequestMapping("/board/write")
+	public String write() {
+		return "BoardController.wite()";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/board/view/{no}")
+	public String view(@PathVariable("no") Long no) {
+		return "BoardController.view(" + no + ")";
+	}
+}
