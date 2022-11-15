5b78827f1862e128bc5651c0606e552c8454c6df
suntaehun <sth7862@naver.com>
2022-11-15 11:33:14 +0900

    @RequestMapping & @RequestParam

---
+++ helloweb03/src/main/java/com/bitacademy/helloweb/controller/UserVo.java
@@ -0,0 +1,88 @@
+package com.bitacademy.helloweb.controller;
+
+public class UserVo {
+	private Long no;
+	private String name;
+	private String email;
+	private String password;
+	private String gender;
+	private String joinDate;
+	/**
+	 * @return the no
+	 */
+	public Long getNo() {
+		return no;
+	}
+	/**
+	 * @param no the no to set
+	 */
+	public void setNo(Long no) {
+		this.no = no;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	/**
+	 * @return the gender
+	 */
+	public String getGender() {
+		return gender;
+	}
+	/**
+	 * @param gender the gender to set
+	 */
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	/**
+	 * @return the joinDate
+	 */
+	public String getJoinDate() {
+		return joinDate;
+	}
+	/**
+	 * @param joinDate the joinDate to set
+	 */
+	public void setJoinDate(String joinDate) {
+		this.joinDate = joinDate;
+	}
+	@Override
+	public String toString() {
+		return "UserVo [no=" + no + ", name=" + name + ", email=" + email + ", password=" + password + ", gender="
+				+ gender + ", joinDate=" + joinDate + "]";
+	}
+	
+}
