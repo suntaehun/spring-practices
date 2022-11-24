60c9a06f0d7631a96289a46ac4bd7d924278fea9
suntaehun <sth7862@naver.com>
2022-11-24 13:58:03 +0900

    jUnit 연습

---
+++ container/src/main/java/com/bitacademy/container/config/user/AppConfig02.java
@@ -0,0 +1,15 @@
+package com.bitacademy.container.config.user;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.bitacademy.container.user.User01;
+
+@Configuration
+public class AppConfig02 {
+	
+	@Bean
+	public User01 user01() {
+		return new User01();
+	}
+}
--- container/src/main/java/com/bitacademy/container/user/test/AppConfigTest.java
+++ container/src/main/java/com/bitacademy/container/user/test/AppConfigTest.java
@@ -5,11 +5,13 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 import com.bitacademy.container.config.user.AppConfig01;
 import com.bitacademy.container.user.User;
+import com.bitacademy.container.user.User01;
 
 public class AppConfigTest {
 
 	public static void main(String[] args) {
-		testAppConfig01();
+//		testAppConfig01();
+		testAppConfig02();
 
 	}
 //	Java Config 01
@@ -19,6 +21,18 @@ public class AppConfigTest {
 		
 		User user = ac.getBean(User.class);
 		System.out.println(user);
+	}
+	
+//	Java Config 02
+//	자바 클래스(Config Class)가 있는 패키지를 전달
+	private static void testAppConfig02() {
+		ApplicationContext ac = new AnnotationConfigApplicationContext("com.bitacademy.container.config.user");
+		
+		User user = ac.getBean(User.class);
+		System.out.println(user);
+		
+		User01 user01 = ac.getBean(User01.class);
+		System.out.println(user01.getName());
 	}
 
 }
