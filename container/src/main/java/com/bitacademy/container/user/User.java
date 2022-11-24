69f864f409b15cd82397f516d004ed48d6250050
suntaehun <sth7862@naver.com>
2022-11-24 10:51:18 +0900

    Ioc Container 2

---
+++ container/src/main/java/com/bitacademy/container/user/User.java
@@ -0,0 +1,38 @@
+package com.bitacademy.container.user;
+
+public class User {
+	private Long no = 0L;
+	private String name = "마이콜";
+	private Friend friend;
+	
+	public User() {
+		
+	}
+	
+	public User(String name) {
+		this.name = name;
+	}
+	
+	public User(Long no, String name) {
+		this.no = no;
+		this.name = name;
+	}
+	
+	public void setNo(Long no) {
+		this.no = no;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setFriend(Friend friend) {
+		this.friend = friend;
+	}
+
+	@Override
+	public String toString() {
+		return "User [no=" + no + ", name=" + name + ", friend=" + friend + "]";
+	}
+	
+}
--- container/src/main/java/com/bitacademy/container/user/test/XmlConfigTest.java
+++ container/src/main/java/com/bitacademy/container/user/test/XmlConfigTest.java
@@ -3,6 +3,7 @@ package com.bitacademy.container.user.test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.bitacademy.container.user.User;
 import com.bitacademy.container.user.User01;
 
 public class XmlConfigTest {
@@ -32,7 +33,41 @@ public class XmlConfigTest {
 	//	XML Bean Configuration(Explicit Configuration)
 	private static void testApplicationContext02() {
 		ApplicationContext ac = new ClassPathXmlApplicationContext("com/bitacademy/container/config/user/applicationContext02.xml");
-
-			
+		
+		User user = null;
+		
+//		id로 빈 가져오기
+		user = (User)ac.getBean("user");
+		System.out.println(user);
+		
+//		name으로 빈 가져오기
+		user = (User)ac.getBean("usr");
+		System.out.println(user);
+		
+//		Type으로 빈 가져오기
+//		1. 같은 타입의 빈이 두 개 이상 있을 때 Type으로 빈 가져오기는 실패한다.
+//		user = ac.getBean(User.class);
+		
+//		2. id + type
+		user = ac.getBean("user2", User.class);
+		System.out.println(user);
+		
+//		3. name + type
+		user = ac.getBean("usr2", User.class);
+		System.out.println(user);
+		
+//		2개 파라미터로 생성된 빈 가져오기 1
+		user = ac.getBean("user3", User.class);
+		System.out.println(user);
+		
+//		2개 파라미터로 생성된 빈 가져오기 2
+		user = ac.getBean("user4", User.class);
+		System.out.println(user);
+		
+//		setter로 초기화한 빈 가져오기1
+		user = ac.getBean("user5", User.class);
+		System.out.println(user);
+		
+		
 		}
 }
